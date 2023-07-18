3fd921d48233653d3f80218bbeef4e78493e4eb0
LAS3RBOY <sahilgupta1107@gmail.com>
2023-07-18 22:12:10 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from 'react-router-dom';
 
 
 function App() {
@@ -45,18 +45,17 @@ function App() {
 
   return (
     <>
-      <Router>
-        <Navbar title="Sahil Gupta" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route exact path='/about' element ={<About className="my-5" />}>
-            </Route>
-            <Route exact path='/' element ={<TextForm heading="Enter your text to analyze" showAlert={showAlert} />}>
-            </Route>
-          </Routes>
-        </div>
-      </Router>
+
+      <Navbar title="Sahil Gupta" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+
+
+        <TextForm heading="Enter your text to analyze" showAlert={showAlert} />
+
+
+      </div>
+
     </>
   );
 }
